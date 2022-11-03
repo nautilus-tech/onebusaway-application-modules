65f52181d958654b49028075178f38160f8e7614
Md Saroar Jahan <saroar.jahan@nautilus-tech.com.au>
2022-11-03 18:14:01 +1030

    Admin deploy (#6)
    * feat: Admin webapp deployment
    
    * chore: Version bumped
    
    * fix: Java mail jar added to tomcat

---
+++ onebusaway-admin-webapp/setenv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sed -i 's@${TRANSIT_DATA_SERVICE_URL}@'"$TRANSIT_DATA_SERVICE_URL"'@g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${DATABASE_HOST}/'"$DATABASE_HOST"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${DATABASE_PORT}/'"$DATABASE_PORT"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${DATABASE_NAME}/'"$DATABASE_NAME"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${DATABASE_USER}/'"$DATABASE_USER"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${DATABASE_PASSWORD}/'"$DATABASE_PASSWORD"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+
+sed -i 's@${SERVER_URL}@'"$SERVER_URL"'@g' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/${DATABASE_HOST}/'"$DATABASE_HOST"'/g' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/${DATABASE_PORT}/'"$DATABASE_PORT"'/g' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/${DATABASE_NAME}/'"$DATABASE_NAME"'/g' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/${DATABASE_USER}/'"$DATABASE_USER"'/g' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/${DATABASE_PASSWORD}/'"$DATABASE_PASSWORD"'/g' ${CATALINA_HOME}/conf/context.xml
+
+sed -i 's/${GOOGLE_MAPS_API_KEY}/'"$GOOGLE_MAPS_API_KEY"'/g' /oba/config.json
+
+sed -i 's/${ADMIN_PASSWORD}/'"$ADMIN_PASSWORD"'/g' ${APP_PATH}/WEB-INF/classes/org/onebusaway/admin/webapp/application-context-webapp.xml
+sed -i 's/${OPERATOR_PASSWORD}/'"$OPERATOR_PASSWORD"'/g' ${APP_PATH}/WEB-INF/classes/org/onebusaway/admin/webapp/application-context-webapp.xml
